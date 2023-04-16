7dc5d2e4c6741122499614f5cd04d4d840fca4ff
rohitashgoswami <rohitash0510@gmail.com>
2023-04-16 14:37:50 +0530

    folder name chenged

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,7 @@
 const search = () =>{
     const searchbox = document.getElementById("search-item").value.toUpperCase();
     const storeitems = document.getElementById("product-list")
-    const product = document.querySelectorAll("product")
+    const product = document.querySelectorAll(".product")
     const pname = storeitems.getElementsByTagName("h2")
 
     for(var i = 0; i < pname.length; i++){
